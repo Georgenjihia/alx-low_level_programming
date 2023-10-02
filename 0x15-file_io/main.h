f0a81c7e350f22860bd35d213a1012072acf2c86
Georgenjihia <njihiageorge@gmail.com>
2023-10-02 06:23:03 -0700

    edit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: holberton.h
- * Auth: George Njihia
+ * Auth: Godswill Kalu
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
@@ -17,5 +17,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H 
+#endif /* MAIN_H */
 
